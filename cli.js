0454c16426f92db673e59ca048fe3b727a734ea3
rmt/src <144435+rmtsrc@users.noreply.github.com>
2022-02-16 08:41:10 +0000

    Bump versions

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const { pollForNewMacOsNotifications } = require('./pollNotifications');
   if (process.argv.includes('--poll')) {
     const interval = process.argv[process.argv.indexOf('--poll') + 1] || 1;
     console.log(`Polling for new notifications every ${interval} second${interval > 1 ? 's' : ''}`);
-    await pollForNewMacOsNotifications(console.log, interval && interval * 1000);
+    pollForNewMacOsNotifications(console.log, interval && interval * 1000);
   } else {
     console.log(await getMacOsNotifications());
   }
